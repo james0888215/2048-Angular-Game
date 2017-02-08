9b037454c80b82db238c702cad5ae813b64c89a0
james0888215 <james@reflexgaming.io>
2017-02-08 22:09:15 +0000

    First push

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twentyfourtyeightApp
+ * @description
+ * # twentyfourtyeightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twentyfourtyeightApp', ['ngAnimate', 'ngAria', 'ngCookies', 'ngMessages', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
